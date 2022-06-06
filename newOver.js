eb0b981268c2c40a1919bddc77d9acfde6e3d25c
Romanqadir <55070560+Romanqadir@users.noreply.github.com>
2022-06-06 16:17:40 +0300

    Update newOver.js

--- newOver.js
+++ newOver.js
@@ -1,37 +1,73 @@
 [
-   {
-    "id":"1",
-    "text":"Most misunderstandings in the world could be avoided if people would simply take the time to ask, What else could this mean?.",
-    "author":" Shannon L. Alder"
-    },
     {
-    "id":"2",
-    "text":"We are dying from overthinking. We are slowly killing ourselves by thinking about everything. Think. Think. Think. You can never trust the human mind anyway. It's a death trap.",
-    "author":"Anthony Hopkins"
-    },
+     "id":"1",
+     "text":"Most misunderstandings in the world could be avoided if people would simply take the time to ask, What else could this mean?.",
+     "author":" Shannon L. Alder"
+     },
+     {
+     "id":"2",
+     "text":"We are dying from overthinking. We are slowly killing ourselves by thinking about everything. Think. Think. Think. You can never trust the human mind anyway. It's a death trap.",
+     "author":"Anthony Hopkins"
+     },
+     {
+     "id":"3",
+     "text":"Thinking has, many a time, made me sad, darling; but doing never did in all my life... My precept is, Do something, my sister, do good if you can; but, at any rate, do something",
+     "author":" Elizabeth Gaskell"
+     },
+     {
+         "id":"4",
+         "text":"To think too much is a disease.",
+         "author":" Fyodor Dostoyevsky,"
+         },
+         {
+             "id":"5",
+             "text":"Don't get too deep, it leads to over thinking, and over thinking leads to problems that doesn't even exist in the first place..",
+             "author":" Jayson Engay"
+             },
+             {
+                 "id":"6",
+                 "text":"The sharpest minds often ruin their lives by overthinking the next step, while the dull win the race with eyes closed.",
+                 "author":"Bethany Brookbank,"
+                 }
+                 , {
+                     "id":"7",
+                     "text":"True saddness is when someone still thinks your the same person after all these years. They brand you because of their own ego, fear and lack of spirituality. What's sadder is when they are Christian.",
+                     "author":"Shannon L. Alder"
+                     }
+     ][
     {
-    "id":"3",
-    "text":"Thinking has, many a time, made me sad, darling; but doing never did in all my life... My precept is, Do something, my sister, do good if you can; but, at any rate, do something",
-    "author":" Elizabeth Gaskell"
-    },
-    {
-        "id":""4",
-        "text":"To think too much is a disease.",
-        "author":" Fyodor Dostoyevsky,"
-        },
-        {
-            "id":"5",
-            "text":"Don't get too deep, it leads to over thinking, and over thinking leads to problems that doesn't even exist in the first place..",
-            "author":" Jayson Engay"
-            },
-            {
-                "id":"6",
-                "text":"The sharpest minds often ruin their lives by overthinking the next step, while the dull win the race with eyes closed.",
-                "author":"Bethany Brookbank,"
-                }
-                , {
-                    "id":"7",
-                    "text":"True saddness is when someone still thinks your the same person after all these years. They brand you because of their own ego, fear and lack of spirituality. What's sadder is when they are Christian.",
-                    "author":"Shannon L. Alder"
-                    }
-    ]
+     "id":"1",
+     "text":"Most misunderstandings in the world could be avoided if people would simply take the time to ask, What else could this mean?.",
+     "author":" Shannon L. Alder"
+     },
+     {
+     "id":"2",
+     "text":"We are dying from overthinking. We are slowly killing ourselves by thinking about everything. Think. Think. Think. You can never trust the human mind anyway. It's a death trap.",
+     "author":"Anthony Hopkins"
+     },
+     {
+     "id":"3",
+     "text":"Thinking has, many a time, made me sad, darling; but doing never did in all my life... My precept is, Do something, my sister, do good if you can; but, at any rate, do something",
+     "author":" Elizabeth Gaskell"
+     },
+     {
+         "id":"4",
+         "text":"To think too much is a disease.",
+         "author":" Fyodor Dostoyevsky,"
+         },
+         {
+             "id":"5",
+             "text":"Don't get too deep, it leads to over thinking, and over thinking leads to problems that doesn't even exist in the first place..",
+             "author":" Jayson Engay"
+             },
+             {
+                 "id":"6",
+                 "text":"The sharpest minds often ruin their lives by overthinking the next step, while the dull win the race with eyes closed.",
+                 "author":"Bethany Brookbank,"
+                 }
+                 , {
+                     "id":"7",
+                     "text":"True saddness is when someone still thinks your the same person after all these years. They brand you because of their own ego, fear and lack of spirituality. What's sadder is when they are Christian.",
+                     "author":"Shannon L. Alder"
+                     }
+     ]
