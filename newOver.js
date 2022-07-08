b5357467c672efc7d80e552476807a0962ed8941
Romanqadir <55070560+Romanqadir@users.noreply.github.com>
2022-07-08 19:17:01 +0300

    update newOver.js

--- newOver.js
+++ newOver.js
@@ -1,37 +1,203 @@
 [
     {
-     "id":"1",
-     "text":"Most misunderstandings in the world could be avoided if people would simply take the time to ask, What else could this mean?.",
-     "author":" Shannon L. Alder"
-     },
-     {
-     "id":"2",
-     "text":"We are dying from overthinking. We are slowly killing ourselves by thinking about everything. Think. Think. Think. You can never trust the human mind anyway. It's a death trap.",
-     "author":"Anthony Hopkins"
-     },
-     {
-     "id":"3",
-     "text":"Thinking has, many a time, made me sad, darling; but doing never did in all my life... My precept is, Do something, my sister, do good if you can; but, at any rate, do something",
-     "author":" Elizabeth Gaskell"
-     },
-     {
-         "id":"4",
-         "text":"To think too much is a disease.",
-         "author":" Fyodor Dostoyevsky,"
-         },
-         {
-             "id":"5",
-             "text":"Don't get too deep, it leads to over thinking, and over thinking leads to problems that doesn't even exist in the first place..",
-             "author":" Jayson Engay"
-             },
-             {
-                 "id":"6",
-                 "text":"The sharpest minds often ruin their lives by overthinking the next step, while the dull win the race with eyes closed.",
-                 "author":"Bethany Brookbank,"
-                 }
-                 , {
-                     "id":"7",
-                     "text":"True saddness is when someone still thinks your the same person after all these years. They brand you because of their own ego, fear and lack of spirituality. What's sadder is when they are Christian.",
-                     "author":"Shannon L. Alder"
-                     }
-     ]
+        "id":"1",
+        "text":"Overthinking ruins you"
+      
+    },
+    {
+        "id":"2",
+        "text":"Less thinking, more living.",
+        "author":" S.R. Crawford"
+    },
+    {
+        "id":"3",
+        "text":"A quiet mind is able to hear intuition over fear."
+        
+    },
+    {
+        "id":"4",
+        "text":"Overthinking keeps you busy but takes you nowhere."
+     
+    },
+    {
+        "id":"5",
+        "text":"Sometimes the worst place to be is in your own head."
+
+    },
+    {
+        "id":"6",
+        "text":"Sleeping is so hard when you can’t stop thinking."
+        
+    },
+    {
+        "id":"7",
+        "text":"Stop overthinking. Whatever happens, happens."
+        
+    },
+    {
+        "id":"8",
+        "text":"Overthinking is the biggest cause of unhappiness."
+       
+    },
+    {
+        "id":"9",
+        "text":"Stop thinking, and end your problems.",
+        "author":"Lao Tzu "
+    },
+    {
+        "id":"10",
+        "text":"Stop overthinking. You can do it.",
+        "author":" Avina Celeste"
+    },
+    {
+        "id":"11",
+        "text":"Overthinking. A powerful drug of mentality.",
+        "author":"A.M.L"
+    },
+    {
+        "id":"12",
+        "text":"To think too much is a disease.",
+        "author":" Fyodor Dostoyevsky "
+    },
+    {
+        "id":"13",
+        "text":"Live in the real world, not in the world of constant thinking."
+   
+    },
+    {
+        "id":"14",
+        "text":"Stop overthinking. You can’t control everything, just let it be."
+   
+    },
+    {
+        "id":"15",
+        "text":"Worry is the interest paid in advance on a debt you may never owe."
+        
+    },
+    {
+        "id":"16",
+        "text":"Thinking will not overcome fear but action will.",
+        "author":" Clement Stone"
+    },
+    {
+        "id":"17",
+        "text":"Trust the overthinker who tells you they love you. They have, most assuredly, thought of every reason not to.",
+        "author":"LK Pilgrim"
+    },
+    {
+        "id":"18",
+        "text":"Worrying is a form of betting against yourself",
+        "author":"Karen Salmansohn "
+    },
+    {
+        "id":"19",
+        "text":"Overthinking – the art of creating problems that weren’t even there."
+   
+    },
+    {
+        "id":"20",
+        "text":"Things won’t get better unless you think better.",
+        "author":"Karen Salmansohn"
+    },
+    {
+        "id":"21",
+        "text":"Worry is interest paid on trouble before it is due.",
+        "author":"William R.Inge"
+    },
+    {
+        "id":"22",
+        "text":"Too much thinking leads to paralysis by analysis.",
+        "author":"Robert Herjavec"
+    },
+    {
+        "id":"23",
+        "text":"Don’t keep things bottled up. It’s not good for you.",
+        "author":" Kody Keplinger"
+    },
+    {
+        "id":"24",
+        "text":"The more you overthink the less you will understand.",
+        "author":"Habeeb Akande"
+    },
+    {
+        "id":"25",
+        "text":"Your addiction to thinking will come back to haunt you.",
+        "author":"Natsume Sōseki"
+    },
+    {
+        "id":"26",
+        "text":"Today I read something that said; “Someone who overthinks is also someone who over loves"
+       
+    },
+    {
+        "id":"27",
+        "text":"Stop worrying about what can go wrong, and get excited about what can go right."
+        
+    },
+    {
+        "id":"28",
+        "text":"Nothing can harm you as much as your own thoughts unguarded",
+        "author":" Buddha"
+    },
+    {
+        "id":"29",
+        "text":"There is nothing in this world that can trouble you as much as your own thoughts."
+   
+    },
+    {
+        "id":"30",
+        "text":"Stop overthinking, put more energy on what you really want to do.",
+        "author":" Amit Ray"
+    },
+    {
+        "id":"31",
+        "text":"Do not overthink it. Breathe and have faith that all will work out for the very best.”"
+        
+    },
+    {
+        "id":"32",
+        "text":"Don’t overthink yourself out of something good!”",
+        "author":" Akosua Dardaine Edwards "
+    },
+    {
+        "id":"33",
+        "text":"A crowded mind leaves no space for a peaceful heart.",
+        "author":"Christine Evangelou "
+    },
+    {
+        "id":"34",
+        "text":"You will never be free until you free yourself from the prison of your own false thoughts."
+      
+    },
+    {
+        "id":"35",
+        "text":"No amount of anxiety can change the future. No amount of regret can change the past."
+       
+    },
+    {
+        "id":"36",
+        "text":"Worrying is blurrying. It stops you from seeing clearly.” – Karen Salmansohn"
+   
+    },
+    {
+        "id":"37",
+        "text":"While you were overthinking, you missed everything worth feeling.",
+        "author":"Nitya Prakash"
+    },
+    {
+        "id":"38",
+        "text":"Action should follow thoughts. If you just get stuck in your thoughts you arrive nowhere."
+        
+    },
+    {
+        "id":"39",
+        "text":"I’m tired of being inside my head. I want to live out here, with you.",
+        "author":"Colleen McCarty"
+    },
+    {
+        "id":"40",
+        "text":"If you spend too much time thinking about a thing, you’ll never get it done.",
+        "author":"Bruce Lee"
+    }
+    
+]
